078cee180c41265eea25ca12e3ef75b9f74948c1
edisonromo-ctrl <edisonromo@gmail.com>
2026-05-09 00:13:27 -0500

    Actualiza cache para equivalencias

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hogar-de-larry-v3';
+const CACHE_NAME = 'hogar-de-larry-v4';
 const APP_FILES = ['./', './index.html', './abstracta.svg', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
